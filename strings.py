f2f8db9152701239c0d7f14de174d03b8bc7dfac
Lawrence <lgnjenga@gmail.com>
2024-02-27 00:10:33 +0300

    Pthyon Crash Course by Traversy Media: Variables, Strings

---
+++ strings.py
@@ -0,0 +1,78 @@
+"""
+    Strings and String Formatting and String Methods
+    - Strings in Python are either surrounded by single or double quotation marks
+"""
+
+name = 'Tosh'
+age = 33
+
+'''
+    1. Concatenate
+'''
+# print('Hello, my name is ' + name + ' and I am ' + str(age) + ' years old')
+
+'''
+    a) String Formatting
+    - Insert Variables into Strings
+'''
+
+'''
+    2. Arguments by Position
+'''
+#print('Hello, my name is {name} and I am {age} years old'.format(name=name, age=age))
+
+'''
+    3. F-Strings (3.6+)
+'''
+print(f'Hello, my name is {name} and I am {age} years old')
+
+'''
+    b) String Methods
+'''
+s = 'helloWorld'
+
+# Capitalize
+print(s.capitalize())
+
+# Make All Uppercase
+print(s.upper())
+
+# Make all Lowercase
+print(s.lower())
+
+# Swap Case
+print(s.swapcase())
+
+# Get Length
+print(len(s))
+
+# Count: Number of occurences of substrings inside the string
+sub = 'o'
+print(s.count(sub))
+
+# Starts with
+print(s.startswith('h'))
+
+# End with
+print(s.endswith('h'))
+
+# Split string into a list/array of individual words
+print(s.split())
+
+# Find the position of a character
+print(s.find('o'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# Is all alphabetic
+print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
+
+
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,56 @@
+# Single line comment
+
+'''
+This is a multiline comment
+Can be single or double quotes
+'''
+
+"""
+Multiline comments are used for docstring
+docstring is used to define a function's Purpose
+"""
+
+"""
+Variable: container for a value, can be of various types
+VARIABLE RULES:
+    - Variables are case sensitive e.g. (name and NAME are two different variables)
+    - Must start with a letter of underscore
+    - can have numbers but not start with them    
+"""
+
+# Single Assignmet of Variables
+# x = 5            # int
+# y = 4.8          # float
+# name = 'Gatonye' # str
+# is_on = False    # bool
+
+# Multiple assignments of Variables
+x, y, name, is_on = (5, 4.8, 'Gatonye', False)
+
+
+# Basic Math
+sum = x + y
+
+# Output the Variables
+print(x, y, name, is_on, sum)
+
+# Types and Type Casting
+# Type Casting: convert one datatype to another
+# int to str
+x = str(x) 
+print(type(x), x)
+
+y = int(y)
+print(type(y), y)
+
+z = float(y)
+print(type(z), z)
+
+
+
+
+
+
+
+
+
